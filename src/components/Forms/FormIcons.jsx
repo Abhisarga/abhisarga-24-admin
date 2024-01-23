1c05f8d9c21042c498a6d930dfb89ad4dc448499
aahnik <daw@aahnik.dev>
2024-01-23 14:23:55 +0530

    add new form icon UpDownArrow

--- src/components/Forms/FormIcons.jsx
+++ src/components/Forms/FormIcons.jsx
@@ -46,3 +46,24 @@ export function PasswordEyeIcon() {
         </>
     );
 }
+
+export function UpDownArrowIcon() {
+    return (
+        <>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-5 w-5 text-gray-500"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+                />
+            </svg>
+        </>
+    );
+}
