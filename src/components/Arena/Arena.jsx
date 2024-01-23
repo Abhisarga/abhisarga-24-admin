004d876fb745a3718bd70934823c96d95014f9c7
aahnik <daw@aahnik.dev>
2024-01-23 15:00:33 +0530

    remove unneccesary padding/margin from Arena

--- src/components/Arena/Arena.jsx
+++ src/components/Arena/Arena.jsx
@@ -2,7 +2,7 @@ export function Arena({ children }) {
     return (
         <>
             <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-                <div className="mx-auto max-w-lg py-16 my-16">
+                <div className="mx-auto max-w-lg">
                     {children}
                 </div>
 
