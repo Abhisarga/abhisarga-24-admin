6e9721860ea8a6f029298ec2b28a63d3acaa5785
aahnik <daw@aahnik.dev>
2024-01-23 14:59:59 +0530

    add vertically stacked radio input component

---
+++ src/components/Forms/RadioGroup.jsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { CheckIcon } from "./FormIcons";
+
+export function RadioGroup({ id, label, children }) {
+    /*
+    pass RadioItem s as children
+    */
+    return (<>
+        <fieldset id={id} className="space-y-4">
+            <legend className="sr-only">{label}</legend>
+
+            {children}
+        </fieldset>
+    </>);
+}
+
+export function RadioItem({ id, groupName, itemKey, value}) {
+    return (<>
+        <div>
+            <input
+                type="radio"
+                name={groupName}
+                value={id}
+                id={id}
+                className="peer hidden [&:checked_+_label_svg]:block"
+            />
+
+            <label
+                htmlFor={id}
+                className="flex cursor-pointer items-center justify-between rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-blue-500"
+            >
+                <div className="flex items-center gap-2">
+                    <CheckIcon />
+
+                    <p className="text-gray-700">{itemKey}</p>
+                </div>
+
+                <p className="text-gray-900">{value}</p>
+            </label>
+        </div>
+
+    </>);
+}
