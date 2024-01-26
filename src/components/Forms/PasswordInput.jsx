86e382dd4041124cdedafa62dc6b7d9f1fbf9332
aahnik <daw@aahnik.dev>
2024-01-26 15:14:23 +0530

    create new component PasswordInput
    todo: add validation

---
+++ src/components/Forms/PasswordInput.jsx
@@ -0,0 +1,11 @@
+import { PasswordEyeIcon } from "./FormIcons";
+import InputBox from "./InputBox";
+
+export default function PasswordInputBox({ id }) {
+    return (
+        <>
+            <InputBox id={'password'} label={'Password'} type={'password'} placeholder={'Enter password'} icon={<PasswordEyeIcon />} />
+            <InputBox id={'password-confirmation'} label={'Confirm Password'} type={'password'} placeholder={'Retype password'} icon={<PasswordEyeIcon />} />
+        </>
+    );
+}
